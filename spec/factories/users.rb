25e8c60a749addb56bd1e089ddf1094fd9206a06
jaykim3217 <102015699+jaykim3217@users.noreply.github.com>
2022-04-20 15:08:55 +0900

    ユーザー管理機能

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {'山'}
+    last_name             {'田'}
+    first_name_kana       {'ヤ'}
+    last_name_kana        {'ナ'}
+    birthday              {'1930-02-02'}
+  end
+end
